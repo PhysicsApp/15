8dd211d1cf34aa5a3d12910add655a247f08ea32
Ricardo Lopez <rikyfocil@gmail.com>
2016-03-09 14:14:58 -0600

    Added storyboard

---
+++ Fifteen/Card.swift
@@ -0,0 +1,13 @@
+//
+//  Card.swift
+//  Fifteen
+//
+//  Created by Ricardo Lopez Focil on 09/03/16.
+//  Copyright © 2016 Ricardo Lopez Focil. All rights reserved.
+//
+
+import UIKit
+
+class Card: NSObject {
+
+}
